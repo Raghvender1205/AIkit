c92a617920025f2d757c9d9585168ac2a75acdb1
Raghvender <raghvender1205@gmail.com>
2021-06-17 16:11:11 +0530

    Profiler GA

---
+++ examples/graveyard/profiling.py
@@ -0,0 +1,62 @@
+import tensorflow.keras
+import tensorflow.keras.optimizers
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.datasets import mnist
+
+
+import aikit.ga.TF.optimizers
+import aikit.profiler
+
+runai.profiler.profile(20, '/projects/runai')
+
+
+num_classes = 10
+epochs = 1
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+print(x_train.shape[0], 'train samples')
+print(x_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+# original
+# batch_size = 10
+# optimizer = keras.optimizers.Adam()
+
+# Run:AI
+batch_size = 2
+optimizer = aikit.ga.TF.optimizers.Adam(steps=5)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizer,
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
